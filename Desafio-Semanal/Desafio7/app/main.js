aa3fa4845a617e8deea177da69ddb3478a82d2a7
Fernanda <86800309+FernandaMamani@users.noreply.github.com>
2023-10-03 19:59:45 -0300

    Update main.js
    desafio 7

--- Desafio-Semanal/Desafio7/app/main.js
+++ Desafio-Semanal/Desafio7/app/main.js
@@ -1,5 +1,14 @@
 console.log(titulo.innerText);
 
-let primerInteg = document.querySelectorAll(dd);
+// let primerInteg = document.querySelectorAll(dd);
 
-alert(dd);
+// alert(dd);
+let datos = document.querySelectorAll("dd");
+
+let integrante = "Integrante 1: "
++ getValor(datos [0].innerHTML, true)
++ getValor(datos[0].innerHTML, true)
++ getValor(datos[0].innerHTML, true)
++ getValor(datos[0].innerHTML, false)
+
+console.log(Integrante1);
