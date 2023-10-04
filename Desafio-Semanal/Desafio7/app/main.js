4902b76fb836edc8f0e1ba8fb5bb66fadeefb987
Fernanda <86800309+FernandaMamani@users.noreply.github.com>
2023-10-04 14:36:15 -0300

    Avance ejercicio 7

--- Desafio-Semanal/Desafio7/app/main.js
+++ Desafio-Semanal/Desafio7/app/main.js
@@ -1,14 +1,38 @@
-console.log(titulo.innerText);
+console.log(titulo.innerHTML);
 
-// let primerInteg = document.querySelectorAll(dd);
 
-// alert(dd);
+
 let datos = document.querySelectorAll("dd");
+console.log(datos);
 
-let integrante = "Integrante 1: "
+let integrante1 = [datos[0].innerHTML, datos[1].innerHTML, datos[2].innerHTML, datos[3].innerHTML];
+let integrante2 = [datos[4].innerHTML, datos[5].innerHTML, datos[6].innerHTML, datos[7].innerHTML];
+console.log(integrante1);
+console.log(integrante2);
+
+/* function integrantesNombres(integrante1,integrante2) {
+    if () {
+
+    }
+}
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let integrante = "Integrante 1: "
 + getValor(datos [0].innerHTML, true)
 + getValor(datos[0].innerHTML, true)
 + getValor(datos[0].innerHTML, true)
 + getValor(datos[0].innerHTML, false)
 
-console.log(Integrante1);
+console.log(Integrante1); */
