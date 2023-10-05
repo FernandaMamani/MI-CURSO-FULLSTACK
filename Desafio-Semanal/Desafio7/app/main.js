61e94696fef134bac5abd7f5b31d9cbe7dd6b5e4
Fernanda <86800309+FernandaMamani@users.noreply.github.com>
2023-10-04 22:46:19 -0300

    Avance del desafio 7

--- Desafio-Semanal/Desafio7/app/main.js
+++ Desafio-Semanal/Desafio7/app/main.js
@@ -1,23 +1,44 @@
 console.log(titulo.innerHTML);
 
 
+let datos1 = document.querySelectorAll("#prim-int");
+console.log(datos1);
 
-let datos = document.querySelectorAll("dd");
-console.log(datos);
+let datos2 = document.querySelectorAll("#seg-int");
+console.log(datos2);
 
-let integrante1 = [datos[0].innerHTML, datos[1].innerHTML, datos[2].innerHTML, datos[3].innerHTML];
-let integrante2 = [datos[4].innerHTML, datos[5].innerHTML, datos[6].innerHTML, datos[7].innerHTML];
+let integrante1 = [datos1[0].innerHTML, datos1[1].innerHTML, datos1[2].innerHTML, datos1[3].innerHTML];
+let integrante2 = [datos2[0].innerHTML, datos2[1].innerHTML, datos2[2].innerHTML, datos2[3].innerHTML];
 console.log(integrante1);
 console.log(integrante2);
 
-/* function integrantesNombres(integrante1,integrante2) {
-    if () {
+ let nombreInt1 = [];
+ let apellidoInt1 = [];
 
-    }
-}
- */
+let nombreInt2 = [];
+let apellidoInt2 = [];
+
+//     if (integrante1[0]!=='' || integrante1[1] !=='' || integrante1[2]!=='' || integrante1[3]!=='') {
+//         nombreInt1.push(integrante1[0], integrante1[1]);
+//         apellidoInt1.push(integrante1[2],integrante1[3]);
+//     }
+
+// if (integrante2[0] !== '' || integrante2[1] !== '' || integrante2[2] !== '' || integrante2[3] !=='') {
+//     nombreInt2.push(integrante2[0], integrante2[1]);
+//     apellidoInt2.push(integrante2[2], integrante2[3]);
+// }
 
 
+
+
+// console.log(nombreInt1);
+// console.log(apellidoInt1);
+ console.log(nombreInt2);
+ console.log(integrante2[2]);
+
+ console.log(integrante2[2].length);
+console.log(integrante2[3].length);
+ console.log(apellidoInt2);
 
 
 
