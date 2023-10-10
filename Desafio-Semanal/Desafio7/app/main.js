5e66acec4ebb6850d32700dacfde4a865605bc35
Fernanda <86800309+FernandaMamani@users.noreply.github.com>
2023-10-10 18:45:15 -0300

    desafio 17 avance

--- Desafio-Semanal/Desafio7/app/main.js
+++ Desafio-Semanal/Desafio7/app/main.js
@@ -7,16 +7,18 @@ console.log(datos1);
 let datos2 = document.querySelectorAll("#seg-int");
 console.log(datos2);
 
-let integrante1 = [datos1[0].innerHTML, datos1[1].innerHTML, datos1[2].innerHTML, datos1[3].innerHTML];
-let integrante2 = [datos2[0].innerHTML, datos2[1].innerHTML, datos2[2].innerHTML, datos2[3].innerHTML];
+let integrante1 = [datos1[0].innerText,datos1[1].innerText,datos1[2].innerText,datos1[3].innerText];
+let integrante2 = [datos2[0].innerText, datos2[1].innerText, datos2[2].innerText, datos2[3].innerText];
+//let integrante1 = [datos1[0].innerHTML, datos1[1].innerHTML, datos1[2].innerHTML, datos1[3].innerHTML];
+//let integrante2 = [datos2[0].innerHTML, datos2[1].innerHTML, datos2[2].innerHTML, datos2[3].innerHTML];
 console.log(integrante1);
 console.log(integrante2);
 
- let nombreInt1 = [];
- let apellidoInt1 = [];
+//  let nombreInt1 = [];
+//  let apellidoInt1 = [];
 
-let nombreInt2 = [];
-let apellidoInt2 = [];
+// let nombreInt2 = [];
+// let apellidoInt2 = [];
 
 //     if (integrante1[0]!=='' || integrante1[1] !=='' || integrante1[2]!=='' || integrante1[3]!=='') {
 //         nombreInt1.push(integrante1[0], integrante1[1]);
@@ -33,14 +35,14 @@ let apellidoInt2 = [];
 
 // console.log(nombreInt1);
 // console.log(apellidoInt1);
- console.log(nombreInt2);
+
+ /* console.log(nombreInt2);
  console.log(integrante2[2]);
 
  console.log(integrante2[2].length);
 console.log(integrante2[3].length);
  console.log(apellidoInt2);
-
-
+ */
 
 
 
