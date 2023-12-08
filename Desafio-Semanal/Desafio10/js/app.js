6aa202b3913c81a01c6e3607bbc2a11e783666b5
Fernanda <86800309+FernandaMamani@users.noreply.github.com>
2023-12-08 15:29:00 -0300

    Desafío 10

---
+++ Desafio-Semanal/Desafio10/js/app.js
@@ -0,0 +1,22 @@
+const displayPreviuosValue = document.querySelector('previous-value');
+const displayCurrentValue = document.querySelector('current-value');
+const btnNumbers = document.querySelectorAll('number');
+const btnOperator = document.querySelectorAll('operator');
+
+function calculator() {
+    function sumar() {
+       return num1 + num2;
+    }
+   function restar() {
+       return num1 - num2;
+   }
+
+    function multiplicar() {
+        return num1 * num2;
+    }
+
+    function multiplicar() {
+        return num1 / num2;
+    }
+
+}
